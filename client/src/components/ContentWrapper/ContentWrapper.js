0758f0731009caa4961f8fad7f6fc294fa6d818c
Camila <contactcamilasanchez@gmail.com>
2023-03-26 13:44:39 -0300

    warning fix

--- client/src/components/ContentWrapper/ContentWrapper.js
+++ client/src/components/ContentWrapper/ContentWrapper.js
@@ -4,13 +4,13 @@ export const ContentWrapper = ({ children, breadcrumb = [] }) => {
   return (
     <div className="content-wrapper">
       {breadcrumb.length > 0 && (
-        <p className="breadcrumb">
+        <div className="breadcrumb">
           {breadcrumb.map(({ name, id }) => (
             <p key={id} className="category">
               {name}
             </p>
           ))}
-        </p>
+        </div>
       )}
       <div className="container">{children}</div>
     </div>
